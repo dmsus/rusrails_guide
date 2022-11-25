d698bf2aa1e23d5c165a1ea7859cf46762c89456
Dmitrii Maslennikov <dmitrymaslennikovs@gmail.com>
2022-11-25 16:38:14 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+	deps = [
+        pkgs.w3m-nographics
+        pkgs.nano
+        pkgs.ruby_3_0
+        pkgs.rubyPackages_3_0.solargraph
+        pkgs.rufo
+        pkgs.sqlite
+	];
+}
